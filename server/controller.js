0b99d2b5ccb3b045e5c2e8764a23520eb342623f
Louisaflor <louisa.yonzon6@gmail.com>
2022-07-29 10:34:31 -0700

    completed the first query for /reviews

--- server/controller.js
+++ server/controller.js
@@ -3,20 +3,41 @@ const model = require('./model.js');
 module.exports = {
 
   getAll: async function(req, res) {
+    var changeSort;
+    if (!req.query.sort || req.query.sort === 'relevant' ) {
+      changeSort = {
+        'helpful' : 'helpfulness',
+        'date' : 'date'
+      }
+    } else if (req.query.sort === 'helpful') {
+      changeSort = 'helpfulness'
+      console.log("CHANGE HERE")
+    } else if (req.query.sort === 'newest') {
+      changeSort = 'date'
+    }
     /**
-     page
-     sort
-     count
-     product_id
+     page = 1
+     sort = 'relevant' 'helpful' 'newest'(date)
+     count = 5
+     product_id = NEED
      */
-    console.log(req.query.page, req.query.sort, req.query.count, req.query.product_id)
+    var param = {
+      'page' :  !req.query.page ? 1 : Number(req.query.page),
+      'sort' : changeSort , //!req.query.sort ? 'relevant' : req.query.sort,
+      'count' : !req.query.count ? 5 : Number(req.query.count),
+      'product_id' : req.query.product_id
+    }
+
+
+    console.log(param)
+    console.log(typeof param.sort === 'object')
     try {
       //invoke the model here
-      const data = await model.getData(req)
+      const data = await model.getData(req, param)
       res.send(data)
     } catch (err) {
       //console.log or send the error here
-      console.log("ERROR", err)
+      res.send(err)
     }
 
   },
--- server/model.js
+++ server/model.js
@@ -2,11 +2,11 @@ const pool = require('../db')
 
 
 module.exports = {
-  getData: function (req) {
-    console.log('im in here: ', req.query.count)
+  getData: function (req, param) {
     return new Promise((resolve, reject) => {
 
-      pool.query((`SELECT
+      if (typeof param.sort === 'object') {
+        pool.query((`SELECT
                     reviews.id,
                     reviews.rating,
                     reviews.summary,
@@ -19,28 +19,66 @@ module.exports = {
                     COALESCE ( json_agg( json_build_object( 'id', photos.id,  'url', photos.url)), '0' ) AS photos
                     FROM reviews
                     LEFT JOIN photos ON reviews.id = photos.reviews_id
-                   WHERE reviews.product_id = 2
+                   WHERE reviews.product_id = ${param.product_id}
                    GROUP BY reviews.id, reviews.rating, reviews.summary, reviews.recommend, reviews.response, reviews.body, reviews.date, reviews.reviewer_name, reviews.helpfulness
-                   limit ${req.query.count}`), (err, data) => {
-        if (err) {
-          reject(err)
-        } else {
-          resolve({
-            'product': req.query.product_id,
-            'page' : req.query.page,
-            'count': req.query.count,
-            'results': data.rows
-          }
+                   ORDER BY reviews.${param.sort.helpful} DESC, reviews.${param.sort.date} DESC
+                   limit ${param.count}`), (err, data) => {
+          if (err) {
+            reject(err)
+          } else {
+            resolve({
+              'product': param.product_id,
+              'page': param.page,
+              'count': param.count,
+              'results': data.rows
+            }
             )
-        }
+          }
 
-      })
+        })
+      } else {
+        pool.query((`SELECT
+                    reviews.id,
+                    reviews.rating,
+                    reviews.summary,
+                    reviews.recommend,
+                    reviews.response,
+                    reviews.body,
+                    reviews.date,
+                    reviews.reviewer_name,
+                    reviews.helpfulness,
+                    COALESCE ( json_agg( json_build_object( 'id', photos.id,  'url', photos.url)), '0' ) AS photos
+                    FROM reviews
+                    LEFT JOIN photos ON reviews.id = photos.reviews_id
+                   WHERE reviews.product_id = ${param.product_id}
+                   GROUP BY reviews.id, reviews.rating, reviews.summary, reviews.recommend, reviews.response, reviews.body, reviews.date, reviews.reviewer_name, reviews.helpfulness
+                   ORDER BY reviews.${param.sort} DESC
+                   limit ${param.count}`), (err, data) => {
+          if (err) {
+            reject(err)
+          } else {
+            resolve({
+              'product': param.product_id,
+              'page': param.page,
+              'count': param.count,
+              'results': data.rows
+            }
+            )
+          }
+
+        })
+
+      }
+
+
+
     })
 
-  },
+
+},
+ }
 
 
-}
 
 //  json_agg (
 //   json_build_object(
