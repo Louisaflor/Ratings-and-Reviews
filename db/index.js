28b31bc4aaa1cf2acf75c575a563eac1a74488a4
Louisaflor <louisa.yonzon6@gmail.com>
2022-08-02 20:00:58 -0700

    npm installed env

--- db/index.js
+++ db/index.js
@@ -1,13 +1,15 @@
 const { Pool, Client } = require("pg");
+require('dotenv').config()
+
 
 //make our connection with posgreSQL
 
 const pool = new Pool({
-  user: 'louisayonzon',
-  host: 'localhost',
-  database: 'postgres',
-  password: '',
-  port: 5432,
+  user: process.env.USER,
+  host: process.env.HOST,
+  database: process.env.DATABASE,
+  password: process.env.PASSWORD,
+  port:  process.env.PORT,
   max: 20,
   idleTimeoutMillis: 30000,
   connectionTimeoutMillis: 2000,
@@ -21,3 +23,15 @@ const pool = new Pool({
 // pool.connect()
 
 module.exports = pool; // pass the connection to the model file
+
+
+// const pool = new Pool({
+//   user: 'louisayonzon',
+//   host: 'localhost',
+//   database: 'postgres',
+//   password: '',
+//   port: 5432,
+//   max: 20,
+//   idleTimeoutMillis: 30000,
+//   connectionTimeoutMillis: 2000,
+// })
