bd33e2628689821ee814c6864de3da564a3717cd
Louisaflor <louisa.yonzon6@gmail.com>
2022-08-21 12:34:45 -0700

    cleaning up code

--- server/controller.js
+++ server/controller.js
@@ -1,109 +1,102 @@
-const model = require('./model.js');
+const model = require("./model.js");
 
 module.exports = {
-
-  getAll: async function(req, res) {
+  getAll: async function (req, res) {
     var param = {
-      'page' :  !req.query.page ? 1 : Number(req.query.page),
-      'sort' : null , //!req.query.sort ? 'relevant' : req.query.sort,
-      'count' : !req.query.count ? 5 : Number(req.query.count),
-      'product_id' : req.query.product_id
-    }
+      page: !req.query.page ? 1 : Number(req.query.page),
+      sort: null, //!req.query.sort ? 'relevant' : req.query.sort,
+      count: !req.query.count ? 5 : Number(req.query.count),
+      product_id: req.query.product_id,
+    };
     var changeSort;
-    if (!req.query.sort || req.query.sort === 'relevant' ) {
-      param.sort = 'helpfulness DESC, reviews.date DESC'
-    } else if (req.query.sort === 'helpful') {
-      param.sort = 'helpfulness DESC'
-    } else if (req.query.sort === 'newest') {
-      param.sort = 'date DESC'
+    if (!req.query.sort || req.query.sort === "relevant") {
+      param.sort = "helpfulness DESC, reviews.date DESC";
+    } else if (req.query.sort === "helpful") {
+      param.sort = "helpfulness DESC";
+    } else if (req.query.sort === "newest") {
+      param.sort = "date DESC";
     }
 
-
-
-    // console.log(param)
-    // console.log(typeof param.sort === 'object')
     try {
       //invoke the model here
-      const data = await model.getData(req, param)
-      res.send(data)
+      const data = await model.getData(req, param);
+      res.send(data);
     } catch (err) {
       //console.log or send the error here
-      res.send(err)
+      res.send(err);
     }
-
   },
 
-  getMeta: async function(req, res) {
-
+  getMeta: async function (req, res) {
     try {
-      const metaData = await model.metaData(req)
+      const metaData = await model.metaData(req);
       // console.log("WHAT IS THIS GOING TO BE: ", metaData)
       // console.log('test:  ',character)
-      res.send(metaData)
+      res.send(metaData);
     } catch (err) {
-      res.send(err)
+      res.send(err);
     }
   },
 
-  post: async function(req, res) {
+  post: async function (req, res) {
     // console.log("GOT IN POST: ", req.body)
     try {
-      const data = await model.postReviews(req.body)
+      const data = await model.postReviews(req.body);
       // console.log("DATA: ", data.rows[0].review_id)
-      return Promise.all(req.body.photos.map((photo) => {model.postPhotos(data.rows[0].review_id, photo)}))
-      .then((result) => {
-        var charactersKeys = Object.keys(req.body.characteristics)
-        var characterValues = Object.keys(req.body.characteristics)
-        // console.log("GIVE ME VALUES: ", charactersKeys, characterValues)
-        return Promise.all(charactersKeys.map((char, index) => {model.postCharacters(data.rows[0].review_id, char, characterValues[index])}))
-        .then((result) => {
-          // console.log("what did I recieve? ", result)
-          res.sendStatus(201)
-
-        }).catch((err) => {
-          // console.log("ERROR IN POSTING CHARACTERS")
+      return Promise.all(
+        req.body.photos.map((photo) => {
+          model.postPhotos(data.rows[0].review_id, photo);
         })
-
-      })
-      .catch((err) => {
-        // console.log("error when posting photos: ", err)
-        res.send(err)
-      })
-
-
+      )
+        .then((result) => {
+          var charactersKeys = Object.keys(req.body.characteristics);
+          var characterValues = Object.keys(req.body.characteristics);
+          // console.log("GIVE ME VALUES: ", charactersKeys, characterValues)
+          return Promise.all(
+            charactersKeys.map((char, index) => {
+              model.postCharacters(
+                data.rows[0].review_id,
+                char,
+                characterValues[index]
+              );
+            })
+          )
+            .then((result) => {
+              // console.log("what did I recieve? ", result)
+              res.sendStatus(201);
+            })
+            .catch((err) => {
+              // console.log("ERROR IN POSTING CHARACTERS")
+            });
+        })
+        .catch((err) => {
+          // console.log("error when posting photos: ", err)
+          res.send(err);
+        });
     } catch (err) {
-     res.send(err)
+      res.send(err);
     }
-
-
   },
 
-  helpful: async function(req, res) {
+  helpful: async function (req, res) {
     // console.log("GIVE ME PARAM: ",req.params.review_id)
 
     try {
-      const data = await model.addHelpful(req.params.review_id)
-      res.sendStatus(201)
+      const data = await model.addHelpful(req.params.review_id);
+      res.sendStatus(201);
     } catch (err) {
-      res.send(err)
+      res.send(err);
     }
-
   },
 
-  report: function() {
+  report: function () {},
 
-  },
-
-  report: async function(req, res) {
-
+  report: async function (req, res) {
     try {
-      const data = await model.reportData(req.params.review_id)
-      res.sendStatus(201)
+      const data = await model.reportData(req.params.review_id);
+      res.sendStatus(201);
     } catch (err) {
-      res.send(err)
+      res.send(err);
     }
-  }
-
-
-
-}
+  },
+};
--- server/model.js
+++ server/model.js
@@ -1,13 +1,10 @@
-const pool = require('../db')
-
+const pool = require("../db");
 
 module.exports = {
   getData: function (req, param) {
     return new Promise((resolve, reject) => {
-
-      // COALESCE ( json_agg( json_build_object( 'id', photos.id,  'url', photos.url)), '[]' ) AS photos
-
-        pool.query((`SELECT
+      pool.query(
+        `SELECT
                     reviews.review_id,
                     reviews.rating,
                     reviews.summary,
@@ -28,52 +25,31 @@ module.exports = {
                    WHERE reviews.product_id = ${param.product_id} AND reviews.reported = false
                    GROUP BY reviews.review_id, reviews.rating, reviews.summary, reviews.recommend, reviews.response, reviews.body, reviews.date, reviews.reviewer_name, reviews.helpfulness
                    ORDER BY reviews.${param.sort}
-                   limit ${param.count}`), (err, data) => {
+                   limit ${param.count}`,
+        (err, data) => {
           if (err) {
-            reject(err)
+            reject(err);
           } else {
             resolve({
-              'product': param.product_id,
-              'page': param.page,
-              'count': param.count,
-              'results': data.rows
-            }
-            )
+              product: param.product_id,
+              page: param.page,
+              count: param.count,
+              results: data.rows,
+            });
           }
-        })
-
-    })
-
-
+        }
+      );
+    });
   },
-
-  /**
-   *
-  ,
-
-    //recommended
-    (SELECT
-      json_object_agg(recommend, count) as recommended
-       FROM (
-       SELECT recommend, COUNT(recommend) AS count
-        FROM reviews
-        WHERE product_id = ${req.product_id}
-        GROUP BY recommend
-        ) recommended
-    );
-
-    //ratings
-    (SELECT  )
-   */
-
 
   metaData: function (req) {
     // console.log('meta in here now?')
-    var storeQuery = {}
-    var product = {'product_id': req.query.product_id}
+    var storeQuery = {};
+    var product = { product_id: req.query.product_id };
     // storeQuery['product_id'] = req.query.product_id
     return new Promise((resolve, reject) => {
-      pool.query((`SELECT
+      pool.query(
+        `SELECT
 
        (SELECT
       json_object_agg(name , json_build_object('id', id , 'value', value)) as characteristics
@@ -106,138 +82,89 @@ module.exports = {
         ) ratings
       )
 
-      `), (err, data) => {
-        if(err) {
-          reject(err)
-        } else {
-          storeQuery = {
-            ...product,
-            ...data.rows[0]
-
+      `,
+        (err, data) => {
+          if (err) {
+            reject(err);
+          } else {
+            storeQuery = {
+              ...product,
+              ...data.rows[0],
+            };
+            // console.log("WHAT WILL THIS STORE LOOK LIKE: ", storeQuery)
+            resolve(storeQuery);
           }
-          // console.log("WHAT WILL THIS STORE LOOK LIKE: ", storeQuery)
-          resolve(storeQuery)
         }
-
-      })
-    })
+      );
+    });
   },
 
-  postReviews: function(req) {
+  postReviews: function (req) {
     // console.log("IN HERE")
     var date = new Date().toISOString();
     return new Promise((resolve, reject) => {
-      pool.query((`INSERT INTO reviews (review_id, product_id, rating, date, summary, body, recommend, reported, reviewer_name, reviewer_email, helpfulness)
+      pool.query(
+        `INSERT INTO reviews (review_id, product_id, rating, date, summary, body, recommend, reported, reviewer_name, reviewer_email, helpfulness)
       VALUES((SELECT setval ('"reviews_review_id_seq"', (SELECT MAX(review_id) FROM reviews)+1)), ${req.product_id}, ${req.rating}, '${date}', '${req.summary}', '${req.body}', ${req.recommend}, false, '${req.name}', '${req.email}', 0) returning *
-                  `), (err, data) => {
-        if (err) {
-          // console.log("ERROR WHEN POSTING DATA: ", err)
-          reject(err)
-        } else {
-          // console.log("GOT IN HERE TOO: ")
-          resolve(data)
+                  `,
+        (err, data) => {
+          if (err) {
+            // console.log("ERROR WHEN POSTING DATA: ", err)
+            reject(err);
+          } else {
+            // console.log("GOT IN HERE TOO: ")
+            resolve(data);
+          }
         }
-      })
-
-    })
+      );
+    });
   },
 
-  postPhotos: function(id, photo) {
+  postPhotos: function (id, photo) {
     return pool.query(`INSERT INTO photos (id, reviews_id, url)
                       VALUES ((SELECT setval ('"photos_id_seq"', (SELECT MAX(id) FROM photos)+1)), ${id}, '${photo}')
-                      returning *`)
+                      returning *`);
   },
 
-  postCharacters: function(review_id, char_id, value) {
+  postCharacters: function (review_id, char_id, value) {
     // console.log("WE GOT IN CHARACTERS: ", review_id, char_id, value)
     return pool.query(`INSERT INTO characteristics_review (id, characteristic_id, review_id, value)
                       VALUES ((SELECT setval ('"characteristics_review_id_seq"', (SELECT MAX(id) FROM characteristics_review)+1)), ${char_id}, ${review_id}, '${value}')
-                      returning *`)
-
+                      returning *`);
   },
 
-  addHelpful: function(req) {
+  addHelpful: function (req) {
     // console.log("DID IT COME?")
     return new Promise((resolve, reject) => {
-      pool.query((`UPDATE reviews SET helpfulness = helpfulness + 1
-                  WHERE review_id = ${req}`), (err) => {
-        if (err) {
-          reject(err)
-        } else {
-          // console.log("COME IN HERE?")
-          resolve('added')
+      pool.query(
+        `UPDATE reviews SET helpfulness = helpfulness + 1
+                  WHERE review_id = ${req}`,
+        (err) => {
+          if (err) {
+            reject(err);
+          } else {
+            // console.log("COME IN HERE?")
+            resolve("added");
+          }
         }
-      })
-    })
+      );
+    });
   },
 
-  reportData: function(req) {
+  reportData: function (req) {
     // console.log("DID IT COME?")
     return new Promise((resolve, reject) => {
-      pool.query((`UPDATE reviews SET reported = true
-                  WHERE review_id = ${req}`), (err) => {
-                    if (err) {
-                      reject(err)
-                    } else {
-                      resolve('added')
-                    }
-                  })
-    })
-  }
-
-
-
-
-
-//replace(___ 'null', '[]')
-
-}
-
-
-// SELECT
-//                   characteristics.id,
-//                   characteristics.name,
-//                   characteristics_review.value
-//                   FROM characteristics
-//                   LEFT JOIN characteristics_review
-//                   ON characteristics.id = characteristics_review.characteristic_id
-//                   WHERE characteristics.product_id = ${req.query.product_id}`), (err, data1) => {
-//         if (err) {
-//           reject(err)
-//         } else {
-//           console.log("GET IN HERE?: ", data1.rows)
-//           var character = {}
-//           for (var item of data1.rows) {
-//             character[item.name] = { 'id': item.id, 'value': item.value }
-//           }
-//           storeQuery['characteristics'] = character //store the characteristics in here
-//           //SELECT count(*) FILTER (WHERE rank = ANY ('{a,b,v}')) AS myvalues
-//           pool.query((`SELECT COUNT(reviews.recommend) FILTER (WHERE reviews.recommend = true AND reviews.product_id = ${req.query.product_id}) AS true,
-//                        COUNT(reviews.recommend) FILTER (WHERE reviews.recommend = false AND reviews.product_id = ${req.query.product_id}) AS false
-//                        FROM reviews`), (err, data2) => {
-//             if (err) {
-//               reject(err)
-//             } else {
-//               storeQuery['recommended'] = data2.rows
-//               pool.query((`SELECT rating FROM reviews WHERE product_id = ${req.query.product_id}`), (err, data3) => {
-//                 if (err) {
-//                   reject(err)
-//                 } else {
-//                   // console.log("data3: ", data3.rows)
-//                   var ratings = { 1: '0', 2: '0', 3: '0', 4: '0', 5: '0' }
-//                   for (item of data3.rows) {
-//                     var num = ratings[item.rating]++
-//                     ratings[item.rating] = ratings[item.rating].toString()
-//                     // console.log(typeof ratings[item.rating])
-//                   }
-//                   storeQuery['ratings'] = ratings
-//                   resolve(storeQuery)
-
-//                 }
-//               })
-//             }
-//           })
-//         }
-//       }
-
-
+      pool.query(
+        `UPDATE reviews SET reported = true
+                  WHERE review_id = ${req}`,
+        (err) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve("added");
+          }
+        }
+      );
+    });
+  },
+};
